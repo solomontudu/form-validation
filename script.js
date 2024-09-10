8296b544d19c4495c2cc37f96d477c799af82776
solomontudu <tukintudu@gmail.com>
2024-09-10 21:06:36 +0530

    added the JS form validation logic, like validate form

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
 const form = document.getElementById("form");
 const password1El = document.getElementById("password1");
 const password2El = document.getElementById("password2");
-const massageContainer = document.querySelector(".message-container");
+const messageContainer = document.querySelector(".message-container");
 const message = document.getElementById("message");
 
 let isValid = false;
+let passwordMatch = false;
 
 function validateForm() {
   // using constraint API
   isValid = form.checkValidity();
   // style main message for an error
-  message.textContent = "Please fill out all fields.";
-  message.style.color = "red";
-  messageContainer.style.borderColor = "red";
+  if (!isValid) {
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  // check to see if passwords match
+  if (password1El.value === password2El.value) {
+    passwordMatch = true;
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordMatch = false;
+    message.textContent = "Make sure passwords match.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColor = "green";
+    password2El.style.borderColor = "green";
+    return;
+  }
+  // form is valid and password match
+  if (isValid && passwordMatch) {
+    message.textContent = "Successfully Registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
 }
 
 function processFormData(e) {
   e.preventDefault();
   // validate form
   validateForm();
+  // submit data if valid
+  if (isValid && passwordMatch) {
+    storeFormData();
+  }
 }
 
 // event listener
